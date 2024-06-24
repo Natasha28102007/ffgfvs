8150956ef8cf32cae8c79995b007202de3b19703
Natasha28102007 <128495861+Natasha28102007@users.noreply.github.com>
2024-06-24 10:49:31 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -2,34 +2,36 @@ const caixaPrincipal = document.querySelector(".caixa-principal");
 const caixaPerguntas = document.querySelector(".caixa-perguntas");
 const caixaAlternativas = document.querySelector(".caixa-Alternativas");
 const caixaResultado = document.querySelector(".caixa-Resultado");
+const textoResultado = document.querySelector(".texto-resultado");
+
 
 const perguntas = [
         {
-            enunciado: "Pergunta 1",
+            enunciado: "Por que tem poluição?",
             alternativas: [
-                "Alternativa 1",
-                "Alternativa 2"
+                "o ser humano provoca poluição ambiental quando suas industrias, por exemplo lançam poluentes no ar",
+                "a poluição não causa mau aos seres humanos "
             ]
         },
         {
-            enunciado: "Pergunta 2",
+            enunciado: "Como preservar árvores e floresta?",
             alternativas: [
-                "Alternativa 1",
-                "Alternativa 2"
+                "reutizando metais e vidros ",
+                "reciclando papeis, jornais e revistas "
             ]
         }
     ]; 
     {
-        enunciado: "Pergunta 3",
+        enunciado: "Derrubar árvores contribui para as cidades tornarem-se  mais quentes?",
         alternativas: [
-            "Alternativa 1",
-            "Alternativa 2"
+            "verdadeiro",
+            "falso"
         ]
         {
-            enunciado: "Pergunta 4",
+            enunciado: "O meio ambiente, bem de uso comum do povo, consistente no equilíbrio ecológico e na higidez do meio e dos recursos naturais, é bem?",
             alternativas: [
-                "Alternativa 1",
-                "Alternativa 2"
+                "comum, geral, difuso, indivisível, indisponivel e impenhorável",
+                "coletivo, divisível e indisponível "
             ]
             {
                 enunciado: "Pergunta 5",
@@ -37,4 +39,14 @@ const perguntas = [
                     "Alternativa 1",
                     "Alternativa 2"
                 ]
-        
+                let atual = 0;
+                let perguntaAtual;
+
+                }
+                function mostraPergunta() {
+                    perguntaAtual = perguntas[atual];
+                    caixaPerguntas.textContent = perguntaAtual.enunciado;
+                }
+                
+                mostraPergunta()
+
